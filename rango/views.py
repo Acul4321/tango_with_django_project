6e3e29a69f110626266ab0ab90d42d209f1653b5
Luca Rogers <2886751r@student.gla.ac.uk>
2025-01-28 14:33:56 +0000

    6.2 index Category implementation

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# Import the Category model
+from rango.models import Category
+
 
 
 def index(request):
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    # Query the database for a list of the top 5 categories 
+    # by the number of likes in descending order.
+    category_list = Category.objects.order_by('-likes')[:5]
+    
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+
+    # Render the response
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
