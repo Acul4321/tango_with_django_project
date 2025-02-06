f20839c694716b28225f8ae68e6b706ec8cb8459
Luca Rogers <2886751r@student.gla.ac.uk>
2025-02-06 18:47:09 +0000

    9.11 auth and permission improvments

--- rango/views.py
+++ rango/views.py
@@ -39,6 +39,7 @@ def show_category(request, category_name_slug):
     # Render the response and return it to the client.
     return render(request, 'rango/category.html', context=context_dict)
 
+@login_required
 def add_category(request):
     form = CategoryForm()
 
@@ -52,6 +53,7 @@ def add_category(request):
         print(form.errors)
     return render(request, 'rango/add_category.html', {'form': form})
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -129,7 +131,7 @@ def user_login(request):
     
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request,'rango/restricted.html')
 
 @login_required
 def user_logout(request):
